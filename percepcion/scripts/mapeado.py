48ef4a7745c14350ef9358a31de0ab8362caeb30
David T.R <davidtejero1999@gmail.com>
2021-01-14 12:39:19 +0100

    Añadido mapa y su segmentacion

---
+++ percepcion/scripts/mapeado.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+import rospy,sys,cv2,roslib
+
+from math import sin,cos,sqrt,pow,atan2
+from sensor_msgs.msg import CameraInfo, Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Pose
+import numpy as np
+
+bridge = CvBridge()
+
+currentImage = np.zeros((256,256,3),dtype="uint8")
+
+#Matriz de parametros intrinsecos de la camara
+K  = np.array([[476.7030836014194, 0.0, 400.5], [0.0, 476.7030836014194, 400.5], [0.0, 0.0, 1.0]])
+
+#Rotacion y traslacion
+phi = np.pi
+psi = -0
+Rx = np.array([[1, 0, 0],[0, cos(phi), -sin(phi)], [0,  sin(phi),  cos(phi)]])
+Rz = np.array([[cos(psi), -sin(psi), 0],[sin(psi),  cos(psi), 0], [0,  0,  1]])
+cRw = np.dot(Rz,Rx)
+ctw = np.array([[0],[0],[4]])
+
+def getImage(data):
+	global currentImage
+	try:
+		currentImage = bridge.imgmsg_to_cv2(data,'bgr8')
+		
+	except CvBridgeError as e:
+		print(e)
+
+
+def newInfo(data):
+	global myCameraInfo
+	myCameraInfo = data
+	rospy.loginfo("\nRecibido nueva info:\n" + str(data))
+
+def getPosicion(imagenOR, h_color, th, mostrar):
+	imagen = imagenOR.copy()
+
+	## BINARIZACION Y APLICACION DE CRITERIO DE COLOR EN HSV
+	hmin = h_color - th[0]
+	hmax = h_color + th[1]
+	frameHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+	frameThreshold = cv2.inRange(frameHSV,(hmin/360.0*179,0,0),(hmax/360.0*179,255,255))
+	im2, contours, hier = cv2.findContours(frameThreshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+	## CALCULO DEL CENTRO DEL BLOB
+	# Usamos los momentos de CV2 para su calculo mas rapido
+	n = 0
+	cX = 0
+	cY = 0
+	# Si hay varios bloques, hacemos la media ponderada por peso
+	M = cv2.moments(frameThreshold)
+	if(M["m00"] != 0):
+		cX += int(M["m10"]/M["m00"])
+		cY += int(M["m01"]/M["m00"])
+		n = n + M["m00"]
+	else:
+		cX, cY = 0, 0
+		print("\nERROR CENTRO")
+
+
+
+	if(mostrar == True):
+		# Pintamos un circulo en la imagen original para ver el
+		cv2.circle(imagen, (cX, cY), 2, (255, 255, 255), -1)
+		#cv2.putText(imagen, "centro", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+		## MOSTRAMOS IMAGENES
+		cv2.imshow("Image", currentImage)
+		cv2.imshow("HSV Image", frameHSV)
+		cv2.imshow("Threshold Image", frameThreshold)
+		cv2.waitKey(1)
+
+	return cX, cY
+
+
+def segmentacionBinaria(imagenOR, h_color, th, mostrar):
+    imagen = imagenOR.copy()
+    hmin = h_color - th[0]
+    hmax = h_color + th[1]
+    frameHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    frameThreshold = cv2.inRange(frameHSV,(hmin/360.0*179,0,0),(hmax/360.0*179,255,255))
+
+
+    if(mostrar == True):
+        # Mostramos la imagen binaria
+		cv2.imshow("Threshold Image", frameThreshold)
+		cv2.waitKey(1)
+
+    return frameThreshold
+
+
+
+def inv_transf(pix_,K,R,t,z):
+	xyz = z*np.dot(np.dot(np.transpose(R),np.linalg.inv(K)),pix_) - np.dot(np.transpose(R), t)
+	
+	"""
+	pix = pix_[0:2,0]
+	print("----pix----")
+	print(pix)
+
+	xy_n = np.dot((pix-K[0:2,2]),np.linalg.inv(K[0:2,0:2]))
+	print("----xy_n----")
+	print(xy_n)
+
+	xyz_c  = np.append(xy_n*z,z)[np.newaxis]
+	print("----xyz_c----")
+	print(xyz_c.T)
+
+	xyz = np.dot(R.T,(xyz_c.T - t))
+	print("----xyz----")
+	"""
+   
+	return xyz
+
+def listener():
+	global myCameraInfo
+	global K
+	global cRw
+	global ctw
+	myCameraInfo = CameraInfo()    
+	rospy.init_node('mapper', anonymous=True)
+	#rospy.Subscriber("camera1/camera_info", CameraInfo, newInfo)
+	rospy.Subscriber("camera1/image_raw", Image, getImage)
+	rate = rospy.Rate(30)
+
+	#Publisher para enviar los datos
+	posicion_pub=rospy.Publisher('camera1/position_orientation', Pose, queue_size = 10)
+	data = Pose()
+
+	while not rospy.is_shutdown():
+        #Segmentamos la imagen actual
+		segmentacionBinaria(currentImage,240,(10,10),True)
+
+		
+		rate.sleep()
+
+
+
+
+if __name__ == '__main__':
+	listener()
